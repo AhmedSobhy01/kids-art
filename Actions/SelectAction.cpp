ca3bfd07ff89f83833161aea326eded7db9646eb
unknown <ahmedamr24680@gmail.com>
2023-12-03 00:38:08 +0200

    fixed select figure bug

--- Actions/SelectAction.cpp
+++ Actions/SelectAction.cpp
@@ -13,24 +13,16 @@ void SelectAction::ReadActionParameters() {
 	Output* pOut = pManager->GetOutput();
 	pOut->PrintMessage("Select: Click on a shape to select.");
 	pIn->GetPointClicked(P.x, P.y);
+	pOut->ClearStatusBar();
 }
 
 void SelectAction::Execute() {
-	Output* pOut = pManager->GetOutput();
-	Input* pIn = pManager->GetInput();
 	ReadActionParameters();
 	CFigure* F = pManager->GetFigure(P.x, P.y);
+	CFigure* S = pManager->GetSelected();
 	if (F != NULL) {
-		F->SetSelected(true);
-		pOut->ClearStatusBar();
-		pManager->GetSelected() = F;
-	}
-	else {
-		int x, y;
-		pOut->PrintMessage("Clicked outside objects so none selected. Click anywhere to continue");
-		pIn->GetPointClicked(x, y);
-		pOut->ClearStatusBar();
-
+		F->SetSelected(S != F);
+		pManager->GetSelected() = F != S ? F : NULL;
 	}
 
 }
