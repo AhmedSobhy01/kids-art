6b06a109661b1458beac28e5163918c41c5eb87f
Ahmed Amr <43810060+AhmedAmrNabil@users.noreply.github.com>
2023-12-04 21:10:36 +0200

    Fix for Issue when Moving Rectangle (#27)

--- Figures/CRectangle.cpp
+++ Figures/CRectangle.cpp
@@ -19,17 +19,21 @@ bool CRectangle::CheckSelected(int x, int y) {
 	return cond1 && cond2;
 }
 void CRectangle::SetCenter(Point c) {
-	Point center = { (Corner1.x - Corner2.x) / 2,(Corner1.y - Corner2.y) / 2 };
+	Point center = { (Corner1.x + Corner2.x) / 2,(Corner1.y + Corner2.y) / 2 };
 	int dy = c.y - center.y;
 	int dx = c.x - center.x;
 	Corner1.x += dx;
+	Corner2.x += dx;
+	Corner1.y += dy;
 	Corner2.y += dy;
 }
 
 bool CRectangle::Validate(Point c) {
-	Point center = { (Corner1.x - Corner2.x) / 2,(Corner1.y - Corner2.y) / 2 };
+	Point center = { (Corner1.x + Corner2.x) / 2,(Corner1.y + Corner2.y) / 2 };
 	int dy = c.y - center.y;
 	int dx = c.x - center.x;
-	return Corner1.y + dy >= UI.ToolBarHeight && Corner2.y + dy >= UI.ToolBarHeight && Corner1.y + dy <= (UI.height - UI.StatusBarHeight) && Corner2.y + dy <= (UI.height - UI.StatusBarHeight);
+	bool cond1 = Corner1.y + dy >= UI.ToolBarHeight && Corner1.y + dy <= (UI.height - UI.StatusBarHeight);
+	bool cond2 = Corner2.y + dy >= UI.ToolBarHeight && Corner2.y + dy <= (UI.height - UI.StatusBarHeight);
+	return cond1 && cond2;
 
 }
