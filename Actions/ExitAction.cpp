81fbf304f52e2b97c844c4507655d601db350c1b
unknown <ahmedamr24680@gmail.com>
2023-12-15 20:40:25 +0200

    removed unused clearall data member

--- Actions/ExitAction.cpp
+++ Actions/ExitAction.cpp
@@ -1,12 +1,12 @@
 #include "ExitAction.h"
+#include "ClearAllAction.h"
 
 ExitAction::ExitAction(ApplicationManager* pApp) :Action(pApp) {
-	clearAll = new ClearAllAction(pApp);
-
 }
 void ExitAction::ReadActionParameters() {};
 
 bool ExitAction::Execute() {
+	ClearAllAction* clearAll = new ClearAllAction(pManager);
 	clearAll->Execute();
 	delete clearAll;
 	return true;
--- Actions/ExitAction.h
+++ Actions/ExitAction.h
@@ -3,11 +3,9 @@
 
 #include "Action.h"
 #include "../ApplicationManager.h"
-#include "ClearAllAction.h"
 class ExitAction :
     public Action
 {
-    ClearAllAction* clearAll;
 public:
     ExitAction(ApplicationManager* pApp);
     void ReadActionParameters();
