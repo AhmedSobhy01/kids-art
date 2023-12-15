7b36f7064f93bb096f49e8dbc69cb465ff90612c
unknown <ahmedamr24680@gmail.com>
2023-12-15 20:33:25 +0200

    add selected figure condition before moving

--- Actions/DragMoveAction.cpp
+++ Actions/DragMoveAction.cpp
@@ -8,38 +8,46 @@ DragMoveAction::DragMoveAction(ApplicationManager* pApp) :Action(pApp) {
 }
 
 void DragMoveAction::ReadActionParameters() {
-	Output* pOut = pManager->GetOutput();
-	pOut->PrintMessage("DragMove: Drag a shape to move");
+
 }
 
 bool DragMoveAction::Execute() {
-	ReadActionParameters();
-	Input* pIn = pManager->GetInput();
+
 	Output* pOut = pManager->GetOutput();
+	Input* pIn = pManager->GetInput();
+	CFigure* SelectedFig = pManager->GetSelected();
+	if (SelectedFig == NULL) {
+		int x, y;
+		pOut->PrintMessage("Error:Select a shape before moving. Click anywhere to continue.");
+		pIn->GetPointClicked(x, y);
+		pOut->ClearStatusBar();
+		return false;
+	}
+	pOut->PrintMessage("DragMove: Drag the selected shape to move");
+
 	bool buttonDown = false;
-	CFigure* Fig;
-	while (!buttonDown || Fig == NULL) {
+	while (!buttonDown || !SelectedFig->CheckSelected(NewCenter.x, NewCenter.y)) {
 		buttonDown = pIn->GetLeftClickState(NewCenter.x, NewCenter.y);
-		if (buttonDown) {
-			Fig = pManager->GetFigure(NewCenter.x, NewCenter.y);
-		}
+
 	}
 
-	OldCenter = Fig->GetCenter();
+	OldCenter = SelectedFig->GetCenter();
 	int dx = OldCenter.x - NewCenter.x;
 	int dy = OldCenter.y - NewCenter.y;
 	int err = 0;
-	while (buttonDown && Fig != NULL) {
+	while (buttonDown) {
 		buttonDown = pIn->GetLeftClickState(NewCenter.x, NewCenter.y);
 		NewCenter.x += dx;
 		NewCenter.y += dy;
-		Fig->SetCenter(NewCenter);
-		err = sqrt(pow(OldCenter.x - NewCenter.x,2) + pow(OldCenter.y - NewCenter.y,2));
-		if(err){
+		SelectedFig->SetCenter(NewCenter);
+		err = sqrt(pow(OldCenter.x - NewCenter.x, 2) + pow(OldCenter.y - NewCenter.y, 2));
+		if (err) { // check if it moved by a pixel before updating interface
 			pManager->UpdateInterface();
 		}
 		OldCenter = NewCenter;
 	}
+	SelectedFig->SetSelected(false);
+	pManager->SetSelected(NULL);
 	pOut->ClearStatusBar();
 
 	return true;
