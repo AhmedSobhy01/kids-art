738493a3d6f122f55372dc5ae5a5e1267bb4c962
Loay Ahmed <84445066+LoayAhmed304@users.noreply.github.com>
2023-12-26 21:45:45 +0200

    :hammer: Fix for Pick By Shape and Color Game (#84)
    * Fixed Pick By Shape and Color mode
    
    * Another fix

--- ApplicationManager.cpp
+++ ApplicationManager.cpp
@@ -314,7 +314,7 @@ int ApplicationManager::CountFigColor(CFigure *Fig)
 
 	for (int i = 0; i < FigList.Size(); i++) // Loop on all figures in the FigList
 	{
-		if (FigList[i]->Type() == Fig->Type() && FigList[i]->GetFillColor() == Fig->GetFillColor()) // If the figure has the same type and color, increment the counter
+		if (*(FigList[i]) == *Fig) // If the figure has the same type and color, increment the counter
 			Counter++;
 	}
 
