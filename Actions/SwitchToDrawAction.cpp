f4cb432abd274746ad80b92a009924d4e7406283
Loay Ahmed <84445066+LoayAhmed304@users.noreply.github.com>
2023-12-26 21:05:40 +0200

    :zap: Fix for Selection when Switching to Play Mode (#82)

--- Actions/SwitchToDrawAction.cpp
+++ Actions/SwitchToDrawAction.cpp
@@ -16,6 +16,9 @@ bool SwitchToDrawAction::Execute()
 	Output *pOut = pManager->GetOutput();
 	pOut->CreateDrawToolBar();
 	pOut->ClearStatusBar();
+	CFigure* SelectedFigure = pManager->GetSelected();
+	if (SelectedFigure != NULL)
+		SelectedFigure->SetSelected(true);
 
 	return true;
 }
